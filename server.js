9a206c89fab40904241566a35234b48fc80b2206
mbcodes <mbcodes@protonmail.com>
2021-07-26 22:48:37 -0700

    update

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const https = require('https');
 const fs = require('fs');
-const db = require('./db.json');
+const { randomUUID } = require('crypto');
+
+// list and stuff for ffmpeg
+let listToMerge = '';
+let listFilePath = `download/${Date.now()}list.txt`;
+let mergedVideoPath = `${randomUUID}.mp4`;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//create an obj to hold ids nad video?
 
 //read contents of db.json
 fs.readFile('./db.json', (error, data) => {
@@ -23,13 +31,29 @@ fs.readFile('./db.json', (error, data) => {
 app.post('/video/fused', (request, response) => {
   const arrayVideoIds = request.body;
 
-  let videosSource = ``;
+  listToMerge = '';
   arrayVideoIds.forEach((videoId) => {
+    // when videoId sent from client matches the one in db, download video
     if (videoId === videos[i].id) {
-      //the source of that id gets downloaded + added to text file
+      videosURL += videos[i].source;
+      videosURL = '';
     }
   });
 });
+
+//download the videos to fuse
+let videosURL = ``;
+function download(url, filepath) {
+  https.get(videosURL, (response) => {
+    const path = `${__dirname}/download`;
+    const filePath = fs.createWriteStream(path);
+    response.pipe(filePath);
+    filePath.on('finish', () => {
+      filePath.close();
+      console.log(`working... 123`);
+    });
+  });
+}
 
 // @GET Route
 app.get('/video/fused/:id', (request, response) => {
