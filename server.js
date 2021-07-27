91ad72956a01f9e74c2df38c09896e0e1adb6acf
mbcodes <mbcodes@protonmail.com>
2021-07-26 20:04:39 -0700

    update

--- server.js
+++ server.js
@@ -2,27 +2,38 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const fs = require('fs');
 const db = require('./db.json');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//@GET root
-app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/index.html');
-});
-
-//@GET show videos to user
-app.get('/video', (request, response) => {
-  response.end(JSON.stringify(db));
+//read contents of db.json
+fs.readFile('./db.json', (error, data) => {
+  if (error) throw error;
+  //JSON object
+  let { videos } = JSON.parse(data);
+  //console.log(videos);
+  for (let i = 0; i < videos.length; i++) {
+    console.log(`${videos[i].id}: ${videos[i].source}`);
+  }
 });
 
 // @POST Route
 app.post('/video/fused', (request, response) => {
-  //
+  const arrayVideoIds = request.body;
+
+  let videosSource = ``;
+  arrayVideoIds.forEach((videoId) => {
+    if (videoId === videos[i].id) {
+      //the source of that id gets downloaded + added to text file
+    }
+  });
 });
 
 // @GET Route
 app.get('/video/fused/:id', (request, response) => {
+  const newVideoId = request.params.id;
   response.send(`future binary response`);
 });
 
