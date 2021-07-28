a6ff5fad992b38b79c616ab23de0637d15fda1aa
mbcodes <mbcodes@protonmail.com>
2021-07-27 23:57:14 -0700

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 const { randomUUID } = require('crypto');
 
 // list and stuff for ffmpeg
@@ -14,16 +15,17 @@ let mergedVideoPath = `${randomUUID}.mp4`;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//create an obj to hold ids nad video?
+//create an obj to hold ids and video?
 
 //read contents of db.json
+const sourceObject = {}; // possibly use, possibly not; don't know yet
 fs.readFile('./db.json', (error, data) => {
   if (error) throw error;
-  //JSON object
+  //JSON object holding the IDs, video src, and img src
   let { videos } = JSON.parse(data);
-  //console.log(videos);
   for (let i = 0; i < videos.length; i++) {
     console.log(`${videos[i].id}: ${videos[i].source}`);
+    //create a hash map of this info? IDK :()
   }
 });
 
@@ -31,26 +33,31 @@ fs.readFile('./db.json', (error, data) => {
 app.post('/video/fused', (request, response) => {
   const arrayVideoIds = request.body;
 
-  listToMerge = '';
   arrayVideoIds.forEach((videoId) => {
     // when videoId sent from client matches the one in db, download video
     if (videoId === videos[i].id) {
       videosURL += videos[i].source;
       videosURL = '';
+      downloadVideo();
     }
   });
 });
 
 //download the videos to fuse
 let videosURL = ``;
-function download(url, filepath) {
+const fileName = path.basename(videosURL);
+
+function downloadVideo(videosURL, callback) {
   https.get(videosURL, (response) => {
-    const path = `${__dirname}/download`;
+    const path = `${__dirname}/downloads/${fileName}`;
     const filePath = fs.createWriteStream(path);
     response.pipe(filePath);
+    filePath.on('error', (err) => {
+      console.log(err);
+    });
     filePath.on('finish', () => {
       filePath.close();
-      console.log(`working... 123`);
+      console.log(`donezo`);
     });
   });
 }
