7d093ccffcc934db9003a9b8e39b5581a10b3e34
mbcodes <mbcodes@protonmail.com>
2021-07-30 21:43:22 -0700

    update again

--- server.js
+++ server.js
@@ -5,57 +5,58 @@ const PORT = process.env.PORT || 5000;
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
-const ffmpeg = require('fluent-ffmpeg');
-const { randomUUID } = require('crypto');
-
-// list and stuff for ffmpeg
-let listToMerge = '';
-let listFilePath = `download/${Date.now()}list.txt`;
-let mergedVideoPath = `${randomUUID}.mp4`;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//create an obj to hold ids and video?
-const sourceObj = {}; // use hashmap to have IDs: URLs
-
-//read contents of db.json
-fs.readFile('./db.json', (error, data) => {
-  if (error) throw error;
-  //JSON object holding the IDs, video src, and img src
-  let { videos } = JSON.parse(data);
-  for (let i = 0; i < videos.length; i++) {
-    const videoId = videos[i].id;
-    if (!(videoId in sourceObj)) {
-      sourceObj[videoId] = videos[i].src;
-    }
-  }
-  console.log(`the object is: \n ${sourceObj}`);
-});
-
 // @POST Route
-app.post('/video/fused', (request, response) => {
-  const arrayVideoIds = request.body;
+app.post('/video/fused', async (request, response) => {
+  const arrayVideoIds = [
+    '25ff984c-e79d-460c-a75f-489e58425656',
+    '67a702b9-1787-4c4b-bee2-b391806b803d',
+  ];
+  //request.body;
+
+  for (let i = 0; i < arrayVideoIds.length; i++) {
+    id += arrayVideoIds[i];
+  }
+
+  //const response = await {};
 });
 
 //download the videos to fuse
-let videosURL = ``;
-const fileName = path.basename(videosURL);
+let id = '';
+let url = '';
 
-function downloadVideo(videosURL) {
-  https.get(videosURL, (response) => {
-    const path = `${__dirname}/downloads/${fileName}`;
-    const filePath = fs.createWriteStream(path);
-    response.pipe(filePath);
-    filePath.on('error', (err) => {
-      console.log(err);
-    });
-    filePath.on('finish', () => {
-      filePath.close();
-      console.log(`donezo`);
-    });
+//read contents of db.json
+function retrieveVidSource() {
+  fs.readFile('./db.json', (error, data) => {
+    if (error) throw error;
+    //JSON object holding the IDs, video src, and img src
+    let { videos } = JSON.parse(data);
+    for (let i = 0; i < videos.length; i++) {
+      if (id === videos[i].id) {
+        url += videos[i].source;
+        console.log(url);
+      }
+    }
   });
 }
+
+async function downloadVideo() {
+  const response = await axios({
+    method: 'GET',
+    url,
+    responseType: 'stream',
+  });
+  const fileName = path.basename(url);
+  const localFilePath = path.resolve(__dirname, 'downloads', fileName);
+  const writeStream = response.data.pipe(fs.createWriteStream(localFilePath));
+  writeStream.on('finish', () => {
+    console.log('Successfully downloaded file!');
+  });
+}
+downloadVideo(url);
 
 // @GET Route
 app.get('/video/fused/:id', (request, response) => {
--- testing.js
+++ testing.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const axios = require('axios');
 
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
-const ffmpeg = require('fluent-ffmpeg');
-const { randomUUID } = require('crypto');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,63 +14,77 @@ app.use(express.urlencoded({ extended: true }));
 
 //read contents of db.json
 //create an obj to hold ids and video?
-const sourceObj = {}; // use hashmap to have IDs: URLs
+//const sourceObj = {}; // use hashmap to have IDs: URLs
 
 //read contents of db.json
-fs.readFile('./db.json', (error, data) => {
-  if (error) throw error;
-  //JSON object holding the IDs, video src, and img src
-  let { videos } = JSON.parse(data);
-  for (let i = 0; i < videos.length; i++) {
-    const videoId = videos[i].id;
-    if (!(videoId in sourceObj)) {
-      sourceObj[videoId] = videos[i].source;
-    }
-  }
-});
+// fs.readFile('./db.json', (error, data) => {
+//   if (error) throw error;
+//   //JSON object holding the IDs, video src, and img src
+//   let { videos } = JSON.parse(data);
+//   for (let i = 0; i < videos.length; i++) {
+//     const videoId = videos[i].id;
+//     const videoSource = videos[i].source;
+//     console.log(videos[i].source);
+//     if (!(videoId in sourceObj)) {
+//       sourceObj[videoId] += videoSource;
+//     }
+//   }
+//   console.log(JSON.stringify(sourceObj));
 
-// @POST Route
-//app.post('/video/fused', (request, response) => {
-const arrayVideoIds = [
-  '25ff984c-e79d-460c-a75f-489e58425656',
-  'd584c1b8-6c87-432c-a14c-db14577e5408',
-]; //request.body;
+// });
+//console.log(JSON.stringify(sourceObj));
 
-arrayVideoIds.forEach((vidIds) => {
-  if (vidIds in sourceObj) {
-    console.log(vidIds);
-    videosURL = sourceObj[vidIds];
-    console.log(videosURL);
-  }
-});
+let id = '';
+let url =
+  'https://video-fuse-footage.s3.us-east-1.amazonaws.com/camera-positioned-on-rocket-during-take-off.mp4';
+
+// // @POST Route
+// //app.post('/video/fused', (request, response) => {
+// const arrayVideoIds = [
+//   //'25ff984c-e79d-460c-a75f-489e58425656',
+
+//   '67a702b9-1787-4c4b-bee2-b391806b803d',
+// ]; //request.body;
+
+// for (let i = 0; i < arrayVideoIds.length; i++) {
+//   id += arrayVideoIds[i];
+// }
+
+// function retrieveVidSource() {
+//   fs.readFile('./db.json', (error, data) => {
+//     if (error) throw error;
+//     //JSON object holding the IDs, video src, and img src
+//     let { videos } = JSON.parse(data);
+//     for (let i = 0; i < videos.length; i++) {
+//       if (id === videos[i].id) {
+//         url += videos[i].source;
+//         console.log(url);
+//       }
+//     }
+//   });
+// }
+// retrieveVidSource(id);
 //});
 
 //download the videos to fuse
-let videosURL = ``;
-const fileName = path.basename(videosURL);
-
-//function downloadVideo(videosURL) {
-https.get(videosURL, (response) => {
-  const path = `${__dirname}/downloads/${fileName}`;
-  const filePath = fs.createWriteStream(path);
-  response.pipe(filePath);
-  filePath.on('error', (err) => {
-    console.log(err);
-  });
-  filePath.on('finish', () => {
-    filePath.close();
-    console.log(`donezo`);
-  });
-});
-//}
-
-// concat videos
-
-// const proc = new ffmpeg({ source: 'title.mp4' })
-//   .mergeAdd('source.mp4')
-//   .mergeToFile('out.mp4', 'myTempFolder/', function () {
-//     console.log('files has been merged succesfully');
-//   });
+// async function downloadVideo() {
+//   try {
+//     const response = await axios({
+//       method: 'GET',
+//       url,
+//       responseType: 'stream',
+//     });
+//     const fileName = path.basename(url);
+//     const localFilePath = path.resolve(__dirname, 'downloads', fileName);
+//     const writeStream = response.data.pipe(fs.createWriteStream(localFilePath));
+//     writeStream.on('finish', () => {
+//       console.log('Successfully downloaded file!');
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// downloadVideo(url);
 
 app.listen(PORT, () => {
   console.log(`your ${PORT} server is running...`);
