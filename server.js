b6be9503a4337714080c20ea64a843e0b1809ffc
mbcodes <mbcodes@protonmail.com>
2021-07-31 19:50:15 -0700

    update sopotecientos

--- server.js
+++ server.js
@@ -1,85 +1,73 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const { videos } = require('./db.json');
 
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
+const ffmpeg = require('fluent-ffmpeg');
 const { v4: uuidv4 } = require('uuid');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let id = '';
-let url = '';
+app.set('view engine', 'ejs');
 
 // @POST Route
-//app.post('/video/fused', async (request, response) => {
-const arrayVideoIds = request.body;
+app.post('/video/fused', async (request, response) => {
+  const arrayVideoIds = request.body.data;
 
-for (let i = 0; i < arrayVideoIds.length; i++) {
-  id += arrayVideoIds[i];
-}
+  const videoSources = retrieveVidSource(arrayVideoIds);
+  const movieFilePaths = await Promise.all(videoSources.map(downloadVideo));
 
-//const response = await {};
-//});
+  const chainedInputs = movieFilePaths.reduce(
+    (result, inputItem) => result.addInput(inputItem),
+    ffmpeg()
+  );
+  const newVidId = uuidv4();
+  chainedInputs.mergeToFile(`./${newVidId}.mp4`, function () {
+    console.log('files has been merged successfully');
+  });
+  response.send(newVidId);
+});
 
 //read contents of db.json + retrieve vid src based off id
-function retrieveVidSource() {
-  fs.readFile('./db.json', (error, data) => {
-    if (error) throw error;
-    //JSON object holding the IDs, video src, and img src
-    let { videos } = JSON.parse(data);
-    for (let i = 0; i < videos.length; i++) {
-      if (id === videos[i].id) {
-        url += videos[i].source;
-        console.log(url);
-      }
-    }
-  });
-}
-retrieveVidSource(id);
+function retrieveVidSource(keycode) {
+  let newArray = [];
 
-async function downloadVideo() {
+  for (let i = 0; i < videos.length; i++) {
+    if (keycode.includes(videos[i].id)) {
+      newArray.push(videos[i].source);
+    }
+  }
+  return newArray;
+}
+
+async function downloadVideo(url) {
   const response = await axios({
     method: 'GET',
     url,
     responseType: 'stream',
   });
+
   const fileName = path.basename(url);
   const localFilePath = path.resolve(__dirname, 'downloads', fileName);
-  const writeStream = response.data.pipe(fs.createWriteStream(localFilePath));
-  writeStream.on('finish', () => {
-    console.log('Successfully downloaded file!');
-  });
-}
-downloadVideo(url);
-
-let videoNames = [];
-
-// read directory for filenames
-fs.readdir(__dirname, (err, files) => {
-  if (err) console.log(err);
-  else {
-    console.log('\nCurrent directory filenames:');
-    files.map((file) => {
-      if (path.extname(file) === '.mp4') {
-        videoNames.push(file);
-      }
+  const end = await new Promise((resolve, reject) => {
+    const writeStream = response.data.pipe(fs.createWriteStream(localFilePath));
+    writeStream.on('finish', () => {
+      console.log('Successfully downloaded file!');
+      resolve(localFilePath); // you can return the path here
     });
-  }
-  console.log(videoNames);
-});
-
-const proc = new ffmpeg(videoNames[0])
-  .mergeAdd(videoNames[1])
-  .mergeToFile(`./${Date.now()}out.mp4`, function () {
-    console.log('files has been merged succesfully');
   });
+  return end;
+}
 
 // @GET Route
 app.get('/video/fused/:id', (request, response) => {
-  const newVideoId = request.params.id;
-  response.send(`future binary response`);
+  const mergedVideoId = request.params.id;
+  console.log(mergedVideoId);
+  response.render('index.ejs', { video: mergedVideoId });
 });
 
 app.listen(PORT, () => {
